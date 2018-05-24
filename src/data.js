52d034e0b2f7e631451775a7409a930345d3e37b
gabriel joubel <gabrieljoubel@MacBook-Pro-de-gabriel.local>
2018-05-24 16:10:41 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,79 @@
+const data = {
+    informations: {
+        firstname: 'Gabriel',
+        lastname: 'Joubel',
+        phone: '07 63 19 67 54',
+        mail: 'joubel.gabriel@gmail.com',
+        driverLicence: 'Permis B',
+    },
+    position: 'Développeur front-end junior',
+    experiences: [
+        {
+            position: 'Ouvrier',
+            organization: 'Ferme de Cano',
+            place: 'Séné',
+            date: 'Saison 2017',
+            details: 'Ventes, entretiens des cultures, cueillette.',
+        },
+        {
+            position: 'Veilleur de nuit',
+            organization: 'Club des dauphins',
+            place: 'La Baule',
+            date: 'Saison 2014',
+            details: 'Surveillance des locaux, entretiens du matériel.',
+        },
+    ],
+    education: [
+        {
+            institution: 'Lycée St Paul',
+            place: 'Vannes',
+            grade: 'Baccalauréat STG',
+            date: '2014',
+        },
+        {
+            institution: 'Lycée Nicolas Brémontier',
+            place: 'Bordeaux',
+            date: '2014 - 2015',
+            grade: 'CPGE 1 année',
+        },
+        {
+            institution: 'Lycée Nicolas Brémontier',
+            place: 'Bordeaux',
+            date: '2015 - 2016',
+            grade: 'CPGE 2 année',
+        },
+        {
+            institution: 'Lycée de La Salle Rennes',
+            place: 'Rennes',
+            date: '2016 - 2017',
+            grade: 'CPGE 2 année',
+        },
+        {
+            institution: 'IUT ',
+            place: 'Vannes',
+            date: '2018',
+            grade: 'GEA 1 année',
+        },
+        {
+            institution: 'Ecole O\' Clock',
+            place: 'Vannes',
+            date: '2018 - 2019',
+            grade: 'Formation de développeur web full-stack',
+        },
+        
+        
+    ],
+    skills: {
+        english: 'Anglais: C1',
+        spanish: 'Espagnol: B2',
+        tools: 'Git',
+        basics: 'HTML5/CSS3/JS',
+        framework: 'React',
+        more: 'Redux',
+
+    },
+    hobbies: {
+        sports: 'Football en club, Snowboard',
+        readings: 'Idriss Aberkane, Olivier Roland, Anthony Beevor, Maxime Chattam, Donato Carrisi'
+    },
+};
